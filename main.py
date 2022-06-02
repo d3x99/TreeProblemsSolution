613197e42d9f10f90d7b81f0964b6d999829ece4
d3x99 <bartekslodyczka16@gmail.com>
2022-06-02 20:27:45 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+
+# Decision Tree Project
+list_count = []
+list_numb = ['first', 'second', 'third']
+x = -1
+y = -1
+z = -1
+output = ''
+dec_count = 0
+decisions_list = []
+steps = []
+print('This is decision tree program')
+problem = input('Tell me what is the problem? \n')
+another_option = 'y'
+end = 0
+while another_option == 'y':
+    if another_option.lower() != 'y':
+        pass
+
+    else:
+        decisions = input('Enter your possible decision: \n')
+        decisions_list.append(decisions)
+        another_option = input('Do you have another option?(y or n): ')
+
+print('\n Now we will define steps for each solution')
+for item in decisions_list:
+    x = x + 1
+    print('Your ' + str(list_numb[x]) + ' problem is ' + item)
+    while True:
+        step = input('Enter step of a problem displayed above: \n'
+                     '(if you are done type "break") \n'
+                     ': ')
+        if step.lower() == 'break':
+            break
+        else:
+            steps.append(step)
+    list_count.append(len(steps))
+    steps.clear()
+
+for decision_1 in decisions_list:
+    y = y + 1
+    print('Decision: ' + decision_1 + ' require ' + str(list_count[y]))
+    for each_step in steps:
+        print(each_step)
+
+
+
